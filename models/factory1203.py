39f7700780ad8de60be018709f91abea595130ea
sunjing <sunjing@sunjingdeMacBook-Pro.local>
2021-12-03 11:10:48 +0800

    sep_actor_critic

---
+++ models/factory1203.py
@@ -0,0 +1,39 @@
+from .model_pomm1203 import ActorNet,CriticNet
+from .model_generic import CNNBase, MLPBase
+from .policy1203 import Policy
+
+
+def create_policy(obs_space, action_space, name='basic', nn_kwargs={}, train=True):
+    nn = None
+    obs_shape = obs_space.shape
+    if name.lower() == 'basic':
+        if len(obs_shape) == 3:
+            nn = CNNBase(obs_shape[0], **nn_kwargs)
+        elif len(obs_shape) == 1:
+            nn = MLPBase(obs_shape[0], **nn_kwargs)
+        else:
+            raise NotImplementedError
+    elif name.lower() == 'pomm':
+        '''nn = PommNet(
+            obs_shape=obs_shape,
+            **nn_kwargs)'''
+        nn1 = ActorNet(
+            obs_shape=obs_shape,
+            **nn_kwargs)
+        nn2 = CriticNet(
+            obs_shape=obs_shape,
+            **nn_kwargs)
+    else:
+        assert False and "Invalid policy name"
+
+    if train:
+        nn1.train()
+        nn2.train()
+    else:
+        nn1.eval()
+        nn2.eval()
+
+    #policy = Policy(nn, action_space=action_space)
+    policy = Policy(nn1, nn2, action_space=action_space)
+
+    return policy
---
+++ models/policy1203.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+from distributions import Categorical, DiagGaussian
+
+
+class Policy(nn.Module):
+    def __init__(self, nn_actor,nn_critic, action_space):
+        super(Policy, self).__init__()
+
+        assert isinstance(nn, torch.nn.Module)
+        #self.nn = nn
+        self.nn_actor = nn_actor
+        self.nn_critic = nn_critic
+
+        if action_space.__class__.__name__ == "Discrete":
+            num_outputs = action_space.n
+            self.dist = Categorical(self.nn_actor.output_size, num_outputs)
+        elif action_space.__class__.__name__ == "Box":
+            num_outputs = action_space.shape[0]
+            self.dist = DiagGaussian(self.nn_actor.output_size, num_outputs)
+        else:
+            raise NotImplementedError
+
+    @property
+    def is_recurrent(self):
+        return self.nn_actor.is_recurrent
+
+    @property
+    def recurrent_hidden_state_size(self):
+        """Size of rnn_hx."""
+        return self.nn_actor.recurrent_hidden_state_size
+
+    def forward(self, inputs, rnn_hxs, masks):
+        raise NotImplementedError
+
+    def act(self, inputs, rnn_hxs, masks, deterministic=False):
+        #value, actor_features, rnn_hxs = self.nn(inputs, rnn_hxs, masks)
+        actor_features, rnn_hxs = self.nn_actor(inputs, rnn_hxs, masks)
+        value = self.nn_critic(inputs, rnn_hxs, masks)
+        dist = self.dist(actor_features)
+
+        if deterministic:
+            action = dist.mode()
+        else:
+            action = dist.sample()
+
+        action_log_probs = dist.log_probs(action)
+        _ = dist.entropy().mean()
+
+        return value, action, action_log_probs, rnn_hxs
+
+    def get_value(self, inputs, rnn_hxs, masks):
+        value= self.nn_critic(inputs, rnn_hxs, masks)
+        return value
+
+    def evaluate_actions(self, inputs, rnn_hxs, masks, action):
+        #actor_features, rnn_hxs = self.nn(inputs, rnn_hxs, masks)
+        actor_features, rnn_hxs = self.nn_actor(inputs, rnn_hxs, masks)
+        value = self.nn_critic(inputs, rnn_hxs, masks)
+        dist = self.dist(actor_features)
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action_log_probs, dist_entropy, rnn_hxs
