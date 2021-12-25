30a58564fc83b6866e331492a4c66baf3bf82161
sunjing <sunjing@HUAWEI_Mate_9-e79bf768450.westwood.local>
2021-12-25 17:20:20 +0800

    add_maac1225

--- algo/__init__.py
+++ algo/__init__.py
@@ -1,5 +1,6 @@
 from .a2c_acktr import A2C_ACKTR
 from .QR_a2c_acktr import QR_A2C_ACKTR
-from .a2c_Team_QR import A2C_Team
+from .a2c_Team_QR import A2C_TeamQR
+from .a2c_team import A2C_Team
 from .ppo import PPO
 from .sil import SIL
