48d8e521bd709c48947c5b8298825445bb52c8c0
sunjing1102628 <sun.jing@ntu.edu.sg>
2022-01-22 10:22:10 +0800

    change_interval

--- arguments.py
+++ arguments.py
@@ -64,7 +64,7 @@ def get_args():
     #                     help='log interval, one log per n updates (default: 10)')
     parser.add_argument('--log-interval', type=int, default=10,
                         help='log interval, one log per n updates (default: 10)')
-    parser.add_argument('--save-interval', type=int, default=100,
+    parser.add_argument('--save-interval', type=int, default=5000,
                         help='save interval, one save per n updates (default: 100)')
     parser.add_argument('--eval-interval', type=int, default=1000,
                         help='eval interval, one eval per n updates (default: None)')
